c29c016789196113114820f8dcc3235b5117f94d
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-17 18:44:11 -0300

    Starting a new project

--- Factorial Number/src/factorial/Factorial1.java
+++ Factorial Number/src/factorial/Factorial1.java
@@ -1,2 +1,14 @@
-package factorial;public class Factorial1 {
+package factorial;
+
+public class Factorial1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Given the variable num to be passed, return the value of its factorial. For example: if num = 4, the
+                program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and
+                the input will always be an integer.
+                """);
+        
+
+
+    }
 }
