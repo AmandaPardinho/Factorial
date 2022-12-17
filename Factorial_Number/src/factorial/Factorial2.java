9936278e1325e40d4b75780c6f1255cac6ecb435
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-17 19:32:09 -0300

    Start solving the code with the conditional structure and the repeat structure

--- Factorial_Number/src/factorial/Factorial2.java
+++ Factorial_Number/src/factorial/Factorial2.java
@@ -3,9 +3,19 @@ package factorial;
 public class Factorial2 {
     public static void main(String[] args) {
         System.out.println(""" 
-                Calculate the factorial value of the numbers 0 to 10.  
-                """);
+                Calculate the factorial value of the numbers 0 to 10.""");
 
 
+        int i = 0;
+        int factorial;
+
+        if(i <= 1){
+            factorial = 1;
+        }
+
+        while(i > 0 && i <= 10){
+            factorial *= i;
+            i++;
+        }
     }
 }
