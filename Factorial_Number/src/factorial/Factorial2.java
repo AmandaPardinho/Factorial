57ffdd452176498cf499f43a077cbf7c32d5024f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-17 19:26:08 -0300

    New factorial exercise started

---
+++ Factorial_Number/src/factorial/Factorial2.java
@@ -0,0 +1,11 @@
+package factorial;
+
+public class Factorial2 {
+    public static void main(String[] args) {
+        System.out.println(""" 
+                Calculate the factorial value of the numbers 0 to 10.  
+                """);
+
+
+    }
+}
